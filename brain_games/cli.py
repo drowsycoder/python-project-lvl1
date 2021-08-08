87f6ffc99cb218fbc517d2427bf7df42770765ae
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-08-08 23:49:52 +0300

    Tune linter rules and bring code to conformity with PEP8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,10 @@
+"""This module is used to operate command-line interface."""
+
 import prompt
 
 
 def welcome_user():
+    """Ask user`s name and greet user."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {name}!'.format(name=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+"""This module is used to operate high level logic."""
+
 from brain_games.cli import welcome_user
 
 
 def main():
+    """High level logic that calls user greeting function."""
     welcome_user()
 
 
